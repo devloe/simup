05a47236818dbe50381d8617ad7c384163a5be07
Juan Pablo Lozano <jplozano6@gmail.com>
2013-04-05 17:38:36 +0200

    Examples page

---
+++ examples.php
@@ -0,0 +1,26 @@
+
+<html>
+<head>
+	<title>Simup - jQuery Plugin</title>
+	<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet" />
+	<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-responsive.min.css" rel="stylesheet">
+	<style>
+		
+	</style>
+</head>
+<body>
+
+	<div class="">
+		
+	</div>
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
+	<script>
+		$("#flipbook").turn({
+			autoCenter: true
+		});
+	</script>
+
+</body>
+</html>
